d270491e6e0f13482d1cab6b20c89ceedbc62cb6
arifkhondokar <arifkhondokar1993@gmail.com>
2024-01-11 12:46:39 +0600

    routting setup, rootLayout, navBar and error page update

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,7 +1,12 @@
 import { Box, Grid } from '@mui/material'
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import './layout.css'
+import ProPic from '../../assets/images/proPic1.png'
+import { IoMdHome } from "react-icons/io";
+import { AiFillMessage } from "react-icons/ai";
+import { IoMdNotifications } from "react-icons/io";
+import { IoSettingsSharp } from "react-icons/io5";
 
 
 const RootLayout = () => {
@@ -10,9 +15,34 @@ const RootLayout = () => {
     <Box>
         <Grid container spacing={0}>
             <Grid item xs={2}>
-                <div className='sidebar'>
-                    <h1>sidebar</h1>
+                <div className='navbar'>
+                    <div className='sideNavbar'>
+                        <div className='proImg'>
+                            <img src={ProPic} alt="proPicture" />
+                        </div>
+                        <div className='navbar'>
+                            <ul>
+                                <li>
+                                    <NavLink to='home'><IoMdHome /></NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/messages'><AiFillMessage /></NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/notification'><IoMdNotifications /></NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/settings'><IoSettingsSharp /></NavLink>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h5>icon</h5>
+                        </div>
+                    </div>
                 </div>
+                
             </Grid>
             <Grid item xs={10}>
                 <Outlet/>
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './error.css'
+import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom'
 
 const Error = () => {
@@ -6,10 +8,12 @@ const Error = () => {
     <>
     <section className='error_section'>
         <div className='error container'>
-            <h1>404 ERROR PAGE</h1>
-            <button>
-                <NavLink className="errorLink" to='/'>Back to Home Page</NavLink>
-            </button>
+            <h1>ERROR : 404</h1>
+            <h3>OPPS! PAGE NOT FOUND</h3>
+            <p>Sorry, the page you're looking for doesn't exist. If you think something is broken, report a problem. </p>
+            <Button variant="contained" disableElevation>
+              <NavLink className="errorPageLink" to='/'>Back to Home</NavLink>
+            </Button>
         </div>
     </section>
     </>
